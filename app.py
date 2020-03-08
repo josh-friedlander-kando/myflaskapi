9640d0366ec94ba3af558609506630a0ccdf2c90
Josh Friedlander <61139724+josh-friedlander-kando@users.noreply.github.com>
2020-03-08 19:10:49 +0200

    adds flag for pred_only, by default returns new thresholds

--- app.py
+++ app.py
@@ -11,19 +11,18 @@ from kando import kando_client
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return 'hello world'
-
-
 @app.route("/train", methods=['POST'])
 def train():
-    point_id, unit_id, start, end, prediction_param = [request.json.get(x) for x in ['point_id', 'unit_id', 'start',
-                                                                                     'end', 'prediction_param']]
+    """
+    accepts POST arguments of point_id, start, end, prediction_param, and optionally unit_id
+    note that the pop() method removes the prediction_param from the dict, so we can pass it to client.get_all()
+    """
+    assert all(x in request.json for x in ['point_id', 'start', 'end', 'prediction_param'])  # unit_id is optional
+    prediction_param = request.json.pop('prediction_param')
     load_dotenv(dirname(__file__), '.env')
     base_url = "https://kando.herokuapp.com"
     client = kando_client.client(base_url, os.getenv('KEY'), os.getenv('SECRET'))
-    data = client.get_all(point_id, unit_id, start, end)
+    data = client.get_all(**request.json)
     print('finished pulling data successfully')
 
     # prep data
@@ -37,20 +36,25 @@ def train():
     m = Prophet(yearly_seasonality=False, weekly_seasonality=True, daily_seasonality=True)
     m.fit(df_prophet)
     # TODO we want to have multiple models, how do we ID and load the correct model?
-    with open(f'/Users/joshfriedlander/Documents/research/models/model_{point_id}.pkl', 'wb+') as f:
+    with open(f'/Users/joshfriedlander/Documents/research/models/model_{request.json["point_id"]}.pkl', 'wb+') as f:
         pickle.dump(m, f)
     return 'finished fitting model'
 
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    point_id, pred_hours = request.json.get('point_id'), request.json.get('pred_hours')
-    with open(f'/Users/joshfriedlander/Documents/research/models/model_{point_id}.pkl', 'rb') as f:
+    # generates forecast df and returns either pred_only, or else range of thresholds derived from yhat_upper/lower
+    with open(f'/Users/joshfriedlander/Documents/research/models/model_{request.json.get("point_id")}.pkl', 'rb') as f:
         m = pickle.load(f)
 
-    future = m.make_future_dataframe(periods=12 * pred_hours, freq='5min')
+    future = m.make_future_dataframe(periods=12 * request.json.get('pred_hours'), freq='5min')
     forecast = m.predict(future)
     print('finished prediction')
-    return forecast.to_dict()
+    if request.json['pred_only']:
+        return forecast[['yhat']].to_dict()
 
-
+    const_2, const_3 = 1.5, 2
+    forecast = forecast.rename(columns={'yhat_upper': 'H1', 'yhat_lower': 'L1'}).copy()
+    forecast['H2'], forecast['H3'] = const_2 * forecast['H1'], const_3 * forecast['H1']
+    forecast['L2'], forecast['L3'] = const_2 * forecast['L1'], const_3 * forecast['L1']
+    return forecast[['yhat', 'H1', 'H2', 'H3', 'L1', 'L2', 'L3']].to_dict()
